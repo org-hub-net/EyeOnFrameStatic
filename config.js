ab7caa3e8a14c6ae75820091ec57ff8ec261113e
TNiopas <48164340+TNiopas@users.noreply.github.com>
2025-07-29 11:03:12 +0300

    no fullscreen video

--- config.js
+++ config.js
@@ -1,19 +1,3 @@
-/*const configModule = await import(window.location.hostname !== '127.0.0.1' ? './config.prod.js' : './config.dev.js');
-const config = configModule.default;
-
-document.querySelectorAll('.cloud-images').forEach(img => {
-  const filename = img.getAttribute('data-src');
-  img.src = `${config.imagesBasePath}${filename}`;
-});
-document.querySelectorAll('.cloud-media').forEach(img => {
-  const filename = img.getAttribute('data-src');
-  img.src = `${config.mediaBasePath}${filename}`;
-});
-
-export default config;
-*/
-
-
 
 const configModule = await import(
   window.location.hostname !== '127.0.0.1'
